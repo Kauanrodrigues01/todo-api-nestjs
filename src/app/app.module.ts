1b6d62bcc3169dd5250fcc6835c0ee495e40848d
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-07-26 22:29:37 -0300

    feat(config): add ConfigModule with environment validation using class-validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,39 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthMiddleware } from 'src/common/middlewares/auth.middleware';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthAdminGuard } from 'src/common/guards/admin.guard';
+import { ConfigModule } from '@nestjs/config';
+import { plainToInstance } from 'class-transformer';
+import { EnvValidationSchema } from 'src/common/config/env.validation';
+import { validateSync } from 'class-validator';
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [
+    TasksModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      validate(config) {
+        // plainToInstance tranforma o objeto config (as variáveis de ambiente) em uma instância de EnvValidationSchema
+        const validatedConfig = plainToInstance(EnvValidationSchema, config, {
+          enableImplicitConversion: true,
+        });
+        // enableImplicitConverion: true é usada para permitir que o class-transformer converta automaticamente os tipos ao transformar o objeto
+
+        // verifica se os valores da instância respeitam as regras de validação declaradas na sua classe. Se algum campo estiver errado, retorna uma lista de erros
+        const errors = validateSync(validatedConfig, {
+          skipMissingProperties: false,
+        });
+
+        if (errors.length > 0) {
+          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+          throw new Error(`.env erro de validação: ${errors}`);
+        }
+
+        return validatedConfig;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
