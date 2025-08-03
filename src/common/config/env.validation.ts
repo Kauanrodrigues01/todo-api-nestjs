ed66338dfe5dfa568d642d9944fbd420a2f27f1f
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-08-03 20:12:50 -0300

    refactor(config): improve .env validation in ConfigModule

--- src/common/config/env.validation.ts
+++ src/common/config/env.validation.ts
@@ -5,4 +5,18 @@ export class EnvValidationSchema {
   @IsString({ message: ValidationMessages.FIELD_STRING('DATABASE_URL') })
   @IsNotEmpty({ message: ValidationMessages.FIELD_REQUIRED('DATABASE_URL') })
   DATABASE_URL: string;
+
+  @IsString({ message: ValidationMessages.FIELD_STRING('JWT_SECRET') })
+  @IsNotEmpty({ message: ValidationMessages.FIELD_REQUIRED('JWT_SECRET') })
+  JWT_SECRET: string;
+
+  @IsString({ message: ValidationMessages.FIELD_STRING('JWT_TTL') })
+  @IsNotEmpty({ message: ValidationMessages.FIELD_REQUIRED('JWT_TTL') })
+  JWT_TTL: string;
+
+  @IsString({ message: ValidationMessages.FIELD_STRING('JWT_TOKEN_ISSUER') })
+  @IsNotEmpty({
+    message: ValidationMessages.FIELD_REQUIRED('JWT_TOKEN_ISSUER'),
+  })
+  JWT_TOKEN_ISSUER: string;
 }
